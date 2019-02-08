79bf2c704746905bc309cbb4c6f8937beab5acaa
ll <myrosicky@163.com>
2019-02-08 10:48:12 +0800

    +pcf implementation

--- eureka-server/src/main/java/org/ll/eurekaServer/startEurekaServer.java
+++ eureka-server/src/main/java/org/ll/eurekaServer/startEurekaServer.java
@@ -2,12 +2,18 @@ package org.ll.eurekaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class startEurekaServer {
+public class startEurekaServer extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(startEurekaServer.class);
+	}
 	public static void main(String[] args){
 		SpringApplication.run(startEurekaServer.class, args);
 	}
